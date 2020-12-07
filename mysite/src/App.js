0a534b99a2807cd9f29c3f7b561e07c28694851f
Bob Whitmer <rjwhitmercc@gmail.com>
2020-12-07 12:59:36 -0700

    react-router

--- mysite/src/App.js
+++ mysite/src/App.js
@@ -1,17 +1,60 @@
 import './App.css';
-import AboutMe from './Components/AboutMe'
-import References from './Components/References'
-import Skills from './Components/Skills'
-import WorkHistory from './Components/WorkHistory'
+import {
+  BrowserRouter as Router,
+  Switch, 
+  Route, 
+  Link, 
+} from 'react-router-dom';
+import AboutMe from './Components/AboutMe';
+import References from './Components/References';
+import Skills from './Components/Skills';
+import WorkHistory from './Components/WorkHistory';
 
 function App() {
   return (
-    <div className="App">
-      <AboutMe />
-      <References />
-      <Skills />
-      <WorkHistory />
-    </div>
+    // <div className="App">
+    //   <AboutMe />
+    //   <References />
+    //   <Skills />
+    //   <WorkHistory />
+    // </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about-me'>About Me</Link>
+            </li>
+            <li>
+              <Link to='/references'>References</Link>
+            </li>
+            <li>
+              <Link to='/skills'>Skills</Link>
+            </li>
+            <li>
+              <Link to='/work-history'>Work History</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path='/about-me'>
+            <AboutMe />
+          </Route>
+          <Route path='/references'>
+            <References />
+          </Route>
+          <Route path='/skills'>
+            <Skills />
+          </Route>
+          <Route path='/work-history'>
+            <WorkHistory />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
