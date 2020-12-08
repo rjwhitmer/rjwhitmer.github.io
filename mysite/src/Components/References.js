1235719e7e4692d09413355cde2bc0f8ef5000af
Bob Whitmer <rjwhitmercc@gmail.com>
2020-12-07 20:58:14 -0700

    References content update

--- mysite/src/Components/References.js
+++ mysite/src/Components/References.js
@@ -6,7 +6,14 @@ export default function References(){
             <div id="refer-header">
                 <p>Check out what they're saying about Bob!</p>
             </div>
-            <p>“Would trust him with my chickens and my code base” - Evan</p>
+            <div id='personal-recommendations'>
+                <div class='recommendations'>
+                    <p>“Would trust him with my chickens and my code base” - Evan</p>
+                </div>
+                <div class='recommendations'>
+                    <p>"The top qualities of Bob are his wide breadth of knowledge, his ability to break down complex problems to find solutions, and a beard you could raise a family of birds in." - Pat</p>
+                </div>
+            </div>
         </div>
     )
 }
